85b472ca8a3d517ad4f624d611a51962f0bba8a7
husky <fekhesk@pm.me>
2023-04-23 14:06:34 -0700

    add a new type that will act as a drop-in replace

--- src/tokio.rs
+++ src/tokio.rs
@@ -52,3 +52,49 @@ impl<T> MutexWithTimeout<T> {
         self.inner.try_lock().ok()
     }
 }
+
+/// A version of `MutexWithTimeout` that will automatically panic if the lock is not acquired within the timeout.
+/// Acts as a drop-in replacement for `tokio::sync::Mutex`.
+pub struct MutexWithTimeoutAuto<T> {
+    inner: Mutex<T>,
+    timeout: Duration,
+}
+
+impl<T> MutexWithTimeoutAuto<T> {
+    /// Creates a new `MutexWithTimeoutAuto` with the default timeout.
+    pub fn new(inner: T) -> Self {
+        Self {
+            inner: Mutex::new(inner),
+            timeout: Duration::from_secs(GLOBAL_TOKIO_TIMEOUT.load(std::sync::atomic::Ordering::Relaxed)),
+        }
+    }
+
+    /// Creates a new `MutexWithTimeoutAuto` with the given timeout.
+    pub async fn new_with_timeout(inner: T, timeout: Duration) -> Self {
+        Self {
+            inner: Mutex::new(inner),
+            timeout,
+        }
+    }
+
+    /// Will attempt to lock the inner `std::sync::Mutex`.
+    /// If the lock is not acquired within the timeout, the thread will panic.
+    pub async fn lock(&self) -> MutexGuard<T> {
+        let start = std::time::Instant::now();
+        loop {
+            if let Ok(guard) = self.inner.try_lock() {
+                return guard;
+            }
+
+            if start.elapsed() > self.timeout {
+                break;
+            }
+        }
+        panic!("MutexWithTimeoutAuto lock timed out after {:?}", self.timeout);
+    }
+
+    /// Calls the inner mutex's `try_lock` method.
+    pub async fn try_lock(&self) -> Option<MutexGuard<'_, T>> {
+        self.inner.try_lock().ok()
+    }
+}
